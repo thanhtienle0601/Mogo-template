b24f854b6f09e8c79e4f212f5083ea9c3046cc75
Thành Tiến <thanhtienle0601@gmail.com>
2021-07-04 13:13:11 +0700

    init source code

---
+++ app/scripts/main.js
@@ -0,0 +1,30 @@
+// menu
+const menuToggle = document.querySelector(".header-toggle");
+const menuHeader = document.querySelector(".header-menu");
+const expandClass = "is-expand";
+menuToggle.addEventListener("click", function() {
+    menuHeader.classList.add(expandClass);
+});
+window.addEventListener("click", function(e) {
+    if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
+        menuHeader.classList.remove(expandClass);
+    }
+});
+
+// slick slider
+$(document).ready(function(){
+    $('.quote-container ').slick({
+        autoplay:true,
+        arrows: true,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+              }
+            }
+          ]
+    });
+  });
